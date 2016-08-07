feb7675d69464ff956aab671a54a29042ce43484
Kennet Postigo <kennetfpostigo@gmail.com>
2016-08-07 01:55:24 -0400

    updated dependency with star component, also added placeholder toolbar component.

---
+++ app/components/Card/CardToolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function CardToolbar (props) {
+  return (
+    <div style={styles.toolbar}>
+      <p>Card Toolbar</p>
+    </div>
+  );
+}
+
+const styles = {
+  toolbar: {
+    marginTop: 4,
+    backgroundColor: 'rgb(248, 248, 248)',
+    borderTop: '1px solid #E4E4E4',
+    height: 50
+  }
+}
+
+export default CardToolbar;
